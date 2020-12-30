457a80fb74355ea7d58af8a00bef12ce6d1bc10e
lynx42 <{46925680}+{Kabraxis}@users.noreply.github.com>
2020-12-31 00:20:49 +0100

    Generating Passwords removed: ASCII ranges for upper-, lowercase and digits introduced: several loops to generate password

--- Problems/Generating passwords/src/Main.java
+++ Problems/Generating passwords/src/Main.java
@@ -2,47 +2,43 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        /*
-        A = UPPERCASE ASCII: 65-90
-        B = lowercase ASCII: 97-122
-        C = Digits ASCII: 48-57
-        N = Total amount of symbols
-
-        a + b + c = x
-        filler = n - (a + b + c)
-
-        n = arrayLength
-
-
-
-         */
         Scanner scanner = new Scanner(System.in);
+
         int a = scanner.nextInt();
         int b = scanner.nextInt();
         int c = scanner.nextInt();
         int n = scanner.nextInt();
+        int d = n - (a + b + c);
 
-        String[] password = new String[n];
-        String[] upperCases = { "A", "B", "C", "D", "E", "F",
-                                "G", "H", "I", "J", "K", "L",
-                                "M", "N", "O", "P", "Q", "R",
-                                "S", "T", "U", "V", "W", "X", "Y", "Z" };
-        
-        String[] lowerCases = { "a", "b", "c", "d", "e", "f",
-                                "g", "h", "i", "j", "k", "l",
-                                "m", "n", "o", "p", "q", "r",
-                                "s", "t", "u", "v", "w", "x", "y", "z" };
+        char[] letters = {'A', 'B'};
+        int[] digits = {1, 2};
 
-        String[] digits = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-
+        for (int i = 0, j = 0; j < a; i++, j++) {
+            System.out.print(letters[i]);
+            if (i == 1) {
+                i = -1;
+            }
         }
 
-
-        for (int i = 0; i < a; i++) {
-
-
+        for (int i = 0, j = 0; j < b; i++, j++) {
+            System.out.print(Character.toLowerCase(letters[i]));
+            if (i == 1) {
+                i = -1;
+            }
         }
 
+        for (int i = 0, j = 0; j < c; i++, j++) {
+            System.out.print(digits[i]);
+            if (i == 1) {
+                i = -1;
+            }
+        }
 
+        for (int i = 1, j = 0; j < d; i++, j++) {
+            System.out.print(letters[i]);
+            if (i == 1) {
+                i = -1;
+            }
+        }
     }
 }
