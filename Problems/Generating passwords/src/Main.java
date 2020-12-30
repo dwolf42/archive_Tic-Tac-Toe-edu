24258ff7f706d7462e95e9eaf2dd9a69c575f09b
lynx42 <{46925680}+{Kabraxis}@users.noreply.github.com>
2020-12-30 12:16:33 +0100

    Generating Passwords

---
+++ Problems/Generating passwords/src/Main.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        A = UPPERCASE
+        B = lowercase
+        C = Digits
+        N = Total amount of symbols
+
+        a + b + c = x
+        filler = n - (a + b + c)
+
+        n = arrayLength
+
+
+
+         */
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int n = scanner.nextInt();
+
+        String[] password = new String[n];
+        String[] upperCases = { "A", "B", "C", "D", "E", "F",
+                                "G", "H", "I", "J", "K", "L",
+                                "M", "N", "O", "P", "Q", "R",
+                                "S", "T", "U", "V", "W", "X", "Y", "Z" };
+        
+        String[] lowerCases = { "a", "b", "c", "d", "e", "f",
+                                "g", "h", "i", "j", "k", "l",
+                                "m", "n", "o", "p", "q", "r",
+                                "s", "t", "u", "v", "w", "x", "y", "z" };
+
+        String[] digits = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+        }
+
+
+        for (int i = 0; i < a; i++) {
+
+        }
+
+
+    }
+}
