59a629e992e4aa409dce8c441ceae7085d07fa93
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2021-02-23 21:14:57 +0100

    update

--- Problems/Fill the matrix by numbers/src/Main.java
+++ Problems/Fill the matrix by numbers/src/Main.java
@@ -3,19 +3,25 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         int defineLength = scanner.nextInt();
         int[][] matrix = new int[defineLength][defineLength];
 
-        for (int i = 0; i < defineLength; i++) {
-            for (int j = i; j > 0; j--) {
-               // System.out.print(j + " ");
-                matrix[j][] = j;
+        for (int i = 0; i < matrix.length; i++) {
+            int m = 0;
+            for (int j = i; j >= 0; j--) {
+                matrix[i][m] = j;
+                m++;
             }
-            for (int k = 0; k < defineLength - i; k++) {
-                System.out.print(k + " ");
+            for (int k = 1; k < matrix.length - i; k++) {
+                matrix[i][m] = k;
+                m++;
             }
-            System.out.println("");
+        }
+        for (int p = 0; p < matrix.length; p++) {
+            for (int q = 0; q < matrix.length; q++) {
+                System.out.print(matrix[p][q] + " ");
+            }
+            System.out.println();
         }
     }
 }
