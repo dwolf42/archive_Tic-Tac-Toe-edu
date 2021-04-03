1ce0317f673ee5041f70a83f0e78d2c66e18000f
Kabraxis <{46925680}+{Kabraxis}@users.noreply.github.com>
2021-04-03 23:28:27 +0200

    fixed typo Y -> O (2nd player is O, not Y)

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -43,29 +43,29 @@ public class Main {
         * -> Game not finished: neither side wins and empty cells
         * -> Draw: neither side wins, no more empty cells
         * -> X wins: three X's in a row
-        * -> Y wins: three Y's in a row
-        * -> Impossible: three X's and three Y's in a row,
+        * -> O wins: three O's in a row
+        * -> Impossible: three X's and three O's in a row,
         * or more X's than O's or vice versa - ratio should be 1 or 0, 2 or more is impossible
         * */
 
         //Impossible: ratio X to O
         int amountX = 0;
-        int amountY = 0;
+        int amountO = 0;
         for (String[] strings : gameBoard) {
             for (int j = 0; j < gameBoard[1].length; j++) {
                 if (strings[j].equals("X")) {
                     amountX++;
                 }
-                if (strings[j].equals("Y")) {
-                    amountY++;
+                if (strings[j].equals("O")) {
+                    amountO++;
                 }
             }
         }
 
-        boolean impossible = amountX - amountY !=0 || amountX - amountY !=1;
+        boolean impossible = amountX - amountO !=0 || amountX - amountO !=1;
 
-        //Impossible: three X's and three Y's in a row
-
+        //Impossible: three X's and three O's in a row
+        //results form concurrency of X and O
 
 
 
