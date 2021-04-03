dd25fd01aedfa533e9d07e70f70924b3bcfe4dd8
Kabraxis <{46925680}+{Kabraxis}@users.noreply.github.com>
2021-04-03 23:12:26 +0200

    switched char- to String-type, implement gameStateAnalyzer: impossible ratio X to O

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -10,38 +10,74 @@ public class Main {
     public static void userInput() {
         Scanner scanner = new Scanner(System.in);
 
-        String input = "XOXOXOXXO";
+        String[] partsOfInput = scanner.nextLine().toUpperCase().split("");
         scanner.close();
 
-        char[] charsOfInput = input.toCharArray();
-        printIt(gameBoard(charsOfInput));
+        printIt(gameBoard(partsOfInput));
     }
 
-    public static char[][] gameBoard(char[] charsOfInput) {
-        char[][] board = {{'-', '-', '-', '-', '-', '-', '-', '-', '-'},
+    public static String[][] gameBoard(String[] partsOfInput) {
+        String[][] board = {{"-", "-", "-", "-", "-", "-", "-", "-", "-"},
 //                   i:1 j:2v  i:1 j:4v  i:1 j:6v
-                {'|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+                {"|", " ", "_", " ", "_", " ", "_", " ", "|"},
 //                   i:2 j:2v  i:2 j:4v  i:2 j:6v
-                {'|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+                {"|", " ", "_", " ", "_", " ", "_", " ", "|"},
 //                   i:3 j:2v  i:3 j:4v  i:3 j:6v
-                {'|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
-                {'-', '-', '-', '-', '-', '-', '-', '-', '-'}
+                {"|", " ", "_", " ", "_", " ", "_", " ", "|"},
+                {"-", "-", "-", "-", "-", "-", "-", "-", "-"}
         };
 
-        int indexOnCharsOfInput = 0;
+        int indexOnPartOfInput = 0;
         for (int i = 1; i <= 3; i++) {
             for (int j = 2; j < 7; j += 2) {
-                board[i][j] = charsOfInput[indexOnCharsOfInput];
-                indexOnCharsOfInput++;
+                board[i][j] = partsOfInput[indexOnPartOfInput];
+                indexOnPartOfInput++;
             }
         }
         return board;
     }
 
-    public static void printIt(char[][] charsOfBoard) {
-        for (char[] characters : charsOfBoard) {
-            for (char chars : characters) {
-                System.out.print(chars);
+    public static void gameStateAnalyzer (String[][] gameBoard) {
+        /*getting gameBoard, iterate through board
+        * to determine:
+        * -> Game not finished: neither side wins and empty cells
+        * -> Draw: neither side wins, no more empty cells
+        * -> X wins: three X's in a row
+        * -> Y wins: three Y's in a row
+        * -> Impossible: three X's and three Y's in a row,
+        * or more X's than O's or vice versa - ratio should be 1 or 0, 2 or more is impossible
+        * */
+        String state = null;
+
+        //Impossible: ratio X to O
+        int amountX = 0;
+        int amountY = 0;
+        for (String[] strings : gameBoard) {
+            for (int j = 0; j < gameBoard[1].length; j++) {
+                if (strings[j].equals("X")) {
+                    amountX++;
+                }
+                if (strings[j].equals("Y")) {
+                    amountY++;
+                }
+            }
+        }
+
+        if (amountX - amountY >= 2 || amountX - amountY <= -2) {
+            state = "impossible";
+        }
+
+        //Impossible: three X's and three Y's in a row
+
+
+
+
+    }
+
+    public static void printIt(String[][] partsOfBoard) {
+        for (String[] strings : partsOfBoard) {
+            for (String stringsi : strings) {
+                System.out.print(stringsi);
             }
             System.out.println("");
         }
