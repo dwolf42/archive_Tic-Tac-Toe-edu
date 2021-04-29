f38229416d9fe35038ad5975221cbf77c41c2664
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2021-04-29 23:51:17 +0200

    experimenting with methods

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -4,20 +4,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        userInput();
-    }
-
-    public static void userInput() {
         Scanner scanner = new Scanner(System.in);
+
         String[] partsOfInput = scanner.nextLine().toUpperCase().split("");
+//                           impossible   XO_XO_XOX
         scanner.close();
 
-        gameBoard(partsOfInput);
-    }
-
-    public static void gameBoard(String[] partsOfInput) {
+        // That's the way to paradise - oh wait, it's just the game board - lol
         String[][] board = {{"-", "-", "-", "-", "-", "-", "-", "-", "-"},
-//                   i:1 j:2v  i:1 j:4v  i:1 j:6v
+//                   i:1 j:2v  i:1 j:4v  i:1 j:6v    <-- 'v' marks the index in which the payer's symbols will be placed
                 {"|", " ", "_", " ", "_", " ", "_", " ", "|"},
 //                   i:2 j:2v  i:2 j:4v  i:2 j:6v
                 {"|", " ", "_", " ", "_", " ", "_", " ", "|"},
@@ -26,6 +21,8 @@ public class Main {
                 {"-", "-", "-", "-", "-", "-", "-", "-", "-"}
         };
 
+        // indexOnPartOfInput; is to count which index (token) of partsOfInput[] should be put into
+        // which position of the board[][]
         int indexOnPartOfInput = 0;
         for (int i = 1; i <= 3; i++) {
             for (int j = 2; j < 7; j += 2) {
@@ -33,12 +30,8 @@ public class Main {
                 indexOnPartOfInput++;
             }
         }
-        printIt(board);
-    }
 
-    public static void gameStateAnalyzer(String[][] gameBoard) {
-        /*getting gameBoard, iterate through board
-         * to determine:
+        /* Iterate through board to determine:
          * -> Game not finished: neither side wins and empty cells
          * -> Draw: neither side wins, no more empty cells
          * -> X wins: three X's in a row
@@ -47,11 +40,12 @@ public class Main {
          * or more X's than O's or vice versa - ratio should be 1 or 0, 2 or more is impossible
          * */
 
-        //Impossible: ratio X to O
+        // Impossible: ratio X to O
+        // If one or the other is more often represented in the array the flag changes
         int amountX = 0;
         int amountO = 0;
-        for (String[] strings : gameBoard) {
-            for (int j = 0; j < gameBoard[1].length; j++) {
+        for (String[] strings : board) {
+            for (int j = 0; j < board[1].length; j++) {
                 if (strings[j].equals("X")) {
                     amountX++;
                 }
@@ -61,22 +55,51 @@ public class Main {
             }
         }
 
-        boolean impossible = amountX - amountO != 0 || amountX - amountO != 1;
+        boolean flagImpossibleRatio = amountX - amountO != 0 || amountX - amountO != 1;
 
-        //Impossible: three X's and three O's in a row
-        //results form concurrency of X and O
-    }
+        // Impossible: three X's and three O's in a row
+        // results form concurrency of X and O
 
-    public static void printIt(boolean impossible, String[][] partsOfBoard) {
-        for (String[] strings : partsOfBoard) {
-            for (String stringsi : strings) {
-                System.out.print(stringsi);
+        // prints board[][] to screen
+        for (String[] boardArrayString : board) {
+            for (String boardString : boardArrayString) {
+                System.out.print(boardString);
             }
             System.out.println("");
         }
-        boolean possi = impossible;
-        System.out.println(possi);
-    }
 
+        System.out.println(flagImpossibleRatio);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
