5d3f1a9aabb0164828e7984ad641812571769087
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2021-06-28 23:28:20 +0200

    X and Y win horizontal, vertical, diagonal simple

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -12,8 +12,8 @@ public class Main {
 
         // That's the way to paradise - oh wait, it's just the game board - lol
         String[][] board = {{" ", " ", " "},
-                            {" ", " ", " "},
-                            {" ", " ", " "}
+                {" ", " ", " "},
+                {" ", " ", " "}
         };
 
         // indexOnPartOfInput; is to count which index (token) of partsOfInput[] should be put into
@@ -35,28 +35,114 @@ public class Main {
          * or more X's than O's or vice versa - ratio should be 1 or 0, 2 or more is impossible
          * */
 
-        // X win conditions
+        // --- X win conditions ---
+
+        // X wins left diagonal
+        if (board[0][0].equals("X") &
+                board[1][1].equals("X") &
+                board[2][2].equals("X")) {
+            System.out.println("X wins!");
+        }
 
         // X wins right diagonal
+        if (board[0][2].equals("X") &
+                board[1][1].equals("X") &
+                board[2][0].equals("X")) {
+            System.out.println("X wins!");
+        }
 
-//        for (int i = 0; i < board.length; i++) {
-//            for (int j = 0; j < board[1].length; j++) {
-               if (board[0][0].equals(board[1][1])) {
-                   System.out.println("I love coding");
-                }
-//            }
-//        }
-/*
+        // X wins horizontal 1,2,3
+        if (board[0][0].equals("X") &
+                board[0][1].equals("X") &
+                board[0][2].equals("X")) {
+            System.out.println("X wins!");
+        }
 
-  0 1 2
-0 x x x
-1 x x x
-2 x x x
+        if (board[1][0].equals("X") &
+                board[1][1].equals("X") &
+                board[1][2].equals("X")) {
+            System.out.println("X wins!");
+        }
+
+        if (board[2][0].equals("X") &
+                board[2][1].equals("X") &
+                board[2][2].equals("X")) {
+            System.out.println("X wins!");
+        }
+
+        // X wins vertical1,2,3
+        if (board[0][0].equals("X") &
+                board[1][0].equals("X") &
+                board[2][0].equals("X")) {
+            System.out.println("X wins!");
+        }
+
+        if (board[0][1].equals("X") &
+                board[1][1].equals("X") &
+                board[2][1].equals("X")) {
+            System.out.println("X wins!");
+        }
+
+        if (board[0][2].equals("X") &
+                board[1][2].equals("X") &
+                board[2][2].equals("X")) {
+            System.out.println("X wins!");
+        }
 
 
+        // --- Y win conditions ---
 
+        // Y wins left diagonal
+        if (board[0][0].equals("Y") &
+                board[1][1].equals("Y") &
+                board[2][2].equals("Y")) {
+            System.out.println("Y wins!");
+        }
 
-*/
+        // X wins right diagonal
+        if (board[0][2].equals("Y") &
+                board[1][1].equals("Y") &
+                board[2][0].equals("Y")) {
+            System.out.println("Y wins!");
+        }
+
+        // X wins horizontal 1,2,3
+        if (board[0][0].equals("Y") &
+                board[0][1].equals("Y") &
+                board[0][2].equals("Y")) {
+            System.out.println("Y wins!");
+        }
+
+        if (board[1][0].equals("Y") &
+                board[1][1].equals("Y") &
+                board[1][2].equals("Y")) {
+            System.out.println("Y wins!");
+        }
+
+        if (board[2][0].equals("Y") &
+                board[2][1].equals("Y") &
+                board[2][2].equals("Y")) {
+            System.out.println("Y wins!");
+        }
+
+        // Y wins vertical1,2,3
+        if (board[0][0].equals("Y") &
+                board[1][0].equals("Y") &
+                board[2][0].equals("Y")) {
+            System.out.println("Y wins!");
+        }
+
+        if (board[0][1].equals("Y") &
+                board[1][1].equals("Y") &
+                board[2][1].equals("Y")) {
+            System.out.println("Y wins!");
+        }
+
+        if (board[0][2].equals("Y") &
+                board[1][2].equals("Y") &
+                board[2][2].equals("Y")) {
+            System.out.println("Y wins!");
+        }
 
 
         // This part is printing the 3x3 board to the screen, also adds the lines around the board
@@ -71,6 +157,24 @@ public class Main {
         System.out.println("---------"); // Lower board line
     }
 }
+
+/*
+  0 1 2 3 4 5 6
+0 * . . * . . *
+1 . * . * . * .
+2 . . * * * . .
+3 * * * * * * *
+4 . . * * * . .
+5 . * . * . * .
+6 * . . * . . *
+
+
+  0 1 2
+0 x x x
+1 x x x
+2 x x x
+
+*/
 
 
 
